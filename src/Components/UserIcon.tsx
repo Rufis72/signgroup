ce33fd7c2d78d2e5907bd82dbf02ede7386e59d9
Rufis72 <milo.games.silverstein@gmail.com>
2025-09-05 19:45:46 -0700

    Initial Commit

---
+++ src/Components/UserIcon.tsx
@@ -0,0 +1,13 @@
+import React, { FC, useState } from 'react'
+
+interface UserIconProps {
+
+}
+
+const UserIcon: FC<UserIconProps>  = ({}) => {
+    const [clicked, setClicked] = useState(false);
+    return (
+        <>
+        </>
+    )
+}
