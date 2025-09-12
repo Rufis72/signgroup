4535389175becbe13bc8b8fcef5e98c16ec8d1cf
Rufis <milo.games.silverstein@gmail.com>
2025-09-11 21:32:12 -0700

    Added top bar to account page

--- src/Account.tsx
+++ src/Account.tsx
@@ -1,7 +1,11 @@
 import { UserProfile } from '@clerk/clerk-react'
+import TopBar from './Components/TopBar'
 
 const AccountPage = () => <div className='cent'>
-     <UserProfile />
+    <TopBar />
+        <div className='flex justify-center items-center'>
+            <UserProfile />
+        </div>
     </div>
 
 export default AccountPage
